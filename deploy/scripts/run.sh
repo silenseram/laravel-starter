7e66226a9f480d96e8b1308bda19a3b8f6e11097
silenseram <ew.mwriter@gmail.com>
2022-01-19 07:56:52 +0300

    fix filenames and add fastcgi.conf

--- deploy/scripts/run.sh
+++ deploy/scripts/run.sh
@@ -1,17 +1,26 @@
 echo "current path is ${PWD}, starting execute run.sh";
+
 echo "running composer";
+composer dump-autoload;
 composer install --working-dir=/app;
+
 bash /app/deploy/wait_for.sh mysql_db:3306 -t 0 -s --;
-echo "database is ready, running migration";
+echo "database is ready";
+
+echo "setting files permissions";
 chown -R www-data:www-data /app;
-echo "permissions set";
-php /app/artisan migrate --force;
+
 echo "starting supervisor";
 service supervisor start;
 echo "supervisor booted";
+
 echo "running cron";
 service cron start;
+
 echo "cron running";
 crontab -u www-data /app/deploy/cron/www-data;
-echo "starting FPM";
+
+echo "running deploy:init command";
+php /app/artisan deploy:init;
+
 php-fpm --nodaemonize --fpm-config /usr/local/etc/php-fpm.d/php-fpm.conf;
